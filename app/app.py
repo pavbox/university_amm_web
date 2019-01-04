dcf3676cc3497354bc652489b369dc203f151202
pavbox <pavboxdev@gmail.com>
2019-01-04 21:44:15 +0300

    added react apps

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, send_from_directory, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/')
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', name='pavbox')
+
+@app.route('/like_button.js')
+def send_js():
+    return send_from_directory('/', 'like_button.js')
