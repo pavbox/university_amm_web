428ecb32576b9b2b2f2b0f5e617478fa6735c45b
pavbox <pavboxdev@gmail.com>
2019-01-23 22:52:21 +0300

    added styles as js

--- app/frontend/src/components/Button/Button.js
+++ app/frontend/src/components/Button/Button.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import './Button.css'
+//import './Button.css'
 
 class Button extends React.Component {
   constructor() {
@@ -8,8 +8,23 @@ class Button extends React.Component {
   }
 
   render() {
+    const style = {
+      'border': 'none',
+      'padding': '20px 35px',
+      'backgroundColor': '#aea',
+      'textAlign': 'center',
+      'fontSize': '18px',
+      'color': '#333',
+
+      'borderRadius': '10px',
+
+      'WebkitAppearance': 'none',
+      'MozAppearance': 'none',
+      'appearance': 'none',
+    }
+
     return (
-      <button className="button">click me, f*cking retard</button>
+      <button style={style} className="button">click me, f*cking retard</button>
     )
   }
 }
