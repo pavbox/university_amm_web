23b1c42f36912cc4f712b8439578e583a4083be5
pavbox <pavboxdev@gmail.com>
2019-01-26 09:34:16 +0300

    added table component

---
+++ app/frontend/src/components/Table/Table.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Table extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div className="table">
+        <div className="row">i'm row</div>
+      <div>
+    )
+  }
+}
+
+export default Table
