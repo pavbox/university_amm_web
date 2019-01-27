dfb87d8ca863847060eafc675784686b90ce8d18
pavbox <pavboxdev@gmail.com>
2019-01-27 16:45:12 +0300

    added container component

---
+++ app/frontend/src/components/Container/Container.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+class Container {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      // how to implement multiple style container?
+      <div className="container"></div>
+    )
+  }
+}
