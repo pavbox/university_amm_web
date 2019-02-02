7821296f7b11b5a948faf675842f6ba88b35f3cb
pavbox <pavboxdev@gmail.com>
2019-02-02 22:54:01 +0300

    added airbnb codestyle, added linter into atom

--- app/frontend/webpack.config.js
+++ app/frontend/webpack.config.js
@@ -1,37 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle-[hash].js'
+    filename: 'bundle-[hash].js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ['babel-loader', 'eslint-loader'],
       },
       {
         test: /\.styl$/,
-        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader' ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader'],
       },
       {
         test: /\.css$/,
-        use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
-      }
-    ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "styles-[hash].css",
-      chunkFilename: "[id].[name].[hash].css"
-    })
-  ]
+      filename: 'styles-[hash].css',
+      chunkFilename: '[id].[name].[hash].css',
+    }),
+  ],
 };
