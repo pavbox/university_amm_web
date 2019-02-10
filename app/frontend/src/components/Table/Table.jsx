7fd0d16b7b939588c261fd0091ebd07aaf0fcfad
pavbox <pavboxdev@gmail.com>
2019-02-10 21:45:57 +0300

    make table component as pure component by react rules

--- app/frontend/src/components/Table/Table.jsx
+++ app/frontend/src/components/Table/Table.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Table.styl';
 
-class Table extends React.Component {
-  constructor() {
-    super();
-  }
+const propTypes = {
+  name: PropTypes.string.isRequired,
+};
 
+class Table extends React.PureComponent {
   render() {
+    const { name } = this.props;
     return (
       <div className="table">
-        <div className="table__row">i'm {this.props.name}</div>
+        <div className="table__row">{name}</div>
       </div>
     );
   }
 }
+
+Table.propTypes = propTypes;
 
 export default Table;
