4c3e6d6dc5b3885f7f68787db21701a3d544f746
pavbox <pavboxdev@gmail.com>
2019-02-17 23:12:01 +0300

    added container component as parent with childrens props

--- app/frontend/src/components/Container/Container.jsx
+++ app/frontend/src/components/Container/Container.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './Container.styl';
 
+const propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
 // how to implement multiple style container?
-const Container = () => <div className="container" />;
+class Container extends React.PureComponent {
+  render() {
+    const { children } = this.props;
+    return (
+      <div className="container">
+        {children}
+      </div>
+    );
+  }
+}
+
+Container.propTypes = propTypes;
 
 export default Container;
