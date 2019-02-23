86eaa133334aba380a033a2a7af61b3fc0aff508
pavbox <pavboxdev@gmail.com>
2019-02-24 00:18:16 +0300

    return back to requirejs imports

--- app/frontend/webpack.common.config.js
+++ app/frontend/webpack.common.config.js
@@ -1,7 +1,6 @@
-
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.jsx',
