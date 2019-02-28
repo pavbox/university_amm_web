6502ec4f4fc3bb799df34dd31eae9522f4d186f3
pavbox <pavboxdev@gmail.com>
2019-03-01 00:34:16 +0300

    integrate mocha link on the page

--- app/frontend/src/index.jsx
+++ app/frontend/src/index.jsx
@@ -7,6 +7,7 @@ import './main.styl';
 import Container from './components/Container';
 import Button from './components/Button';
 import Table from './components/Table';
+import Link from './components/Link';
 
 ReactDOM.render(
   <div>
@@ -14,6 +15,7 @@ ReactDOM.render(
       <h1>Расписание</h1>
       <Table name="Новый день" />
       <Button value="action" />
+      <Link href="https://webpack.js.org/loaders/mocha-loader/" text="mocha" />
     </Container>
   </div>,
   document.getElementById('root'),
