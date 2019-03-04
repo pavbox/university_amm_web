daa3e7ddeb3df8fe8413344eec898780e312549e
pavbox <pavboxdev@gmail.com>
2019-03-04 23:58:37 +0300

    hide unused items

--- app/frontend/src/index.jsx
+++ app/frontend/src/index.jsx
@@ -5,17 +5,15 @@ import 'normalize.css';
 import './main.styl';
 
 import Container from './components/Container';
-import Button from './components/Button';
+// import Button from './components/Button';
 import Table from './components/Table';
-import Link from './components/Link';
+// import Link from './components/Link';
 
 ReactDOM.render(
   <div>
     <Container>
       <h1>Расписание</h1>
       <Table rowItem={{ name: 'pavel', subject: 'math', time: 'monday' }} />
-      <Button value="Cancel" />
-      <Link href="https://webpack.js.org/loaders/mocha-loader/" text="mocha" />
     </Container>
   </div>,
   document.getElementById('root'),
